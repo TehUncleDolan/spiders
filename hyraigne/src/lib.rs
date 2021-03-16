2e5a56bf6ed75275ddf7c79bfdb57c0a976bd1db
TehUncleDolan <the.uncle.dolan@invalid.com>
2021-03-16 15:34:22 +0100

    hyraigne: ensure that the example always compile

--- hyraigne/src/lib.rs
+++ hyraigne/src/lib.rs
@@ -73,3 +73,17 @@ pub use types::{
     Page,
     Series,
 };
+
+// To test examples in the README.
+// From https://github.com/rust-lang/cargo/issues/383#issuecomment-720873790
+#[cfg(doctest)]
+mod test_readme {
+    macro_rules! external_doc_test {
+        ($x:expr) => {
+            #[doc = $x]
+            extern "C" {}
+        };
+    }
+
+    external_doc_test!(include_str!("../README.md"));
+}
